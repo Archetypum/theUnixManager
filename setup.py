52e21b906b22b860e1a21caca5f825bedb9001d9
Kinderfeld <industrialmachine2000@protonmail.com>
2025-01-12 20:09:29 +0300

    small fixes

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ setup(
     long_description=long_description,
     url="https://github.com/Archetypum/theUnixManager-python",
     download_url="https://github.com/Archetypum/archive/v{}.zip".format("1.0.0"),
-    packages=["the_unix_manager"],
+    packages=["theUnixManager"],
     install_requires=[],
     classifiers=[
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later",
-        "Operating System :: UNIX-like",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: Unix",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
     ]
